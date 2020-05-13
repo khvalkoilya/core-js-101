c5e6eb3cf57006829b92dd47970673c3a7265f5a
khvalkoilya <khvalko.ilya@gmail.com>
2020-05-13 22:59:10 +0300

    feat: add regex tasks

--- src/05-regex-tasks.js
+++ src/05-regex-tasks.js
@@ -32,7 +32,8 @@
  * @return {RegExp}
  */
 function getRegexForGuid() {
-  throw new Error('Not implemented');
+  const reg = /^{[\da-f]{8}-[\da-f]{4}-[\da-f]{4}-[\da-f]{4}-[\da-f]{12}}$/i;
+  return new RegExp(reg);
 }
 
 
@@ -54,7 +55,8 @@ function getRegexForGuid() {
  *
  */
 function getRegexForPitSpot() {
-  throw new Error('Not implemented');
+  const reg = /^[^p\s]+|pit/i;
+  return new RegExp(reg);
 }
 
 
@@ -78,8 +80,8 @@ function getRegexForPitSpot() {
  *   'PASSW0RD'.match(validator)  => false
  *   'Pa55'.match(validator) => false
  */
-function getPasswordValidator(/* minLength */) {
-  throw new Error('Not implemented');
+function getPasswordValidator(minLength) {
+  return new RegExp(`^(?=.*[0-9^])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{${minLength},}`);
 }
 
 
